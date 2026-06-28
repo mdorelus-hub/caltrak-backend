0acca5b09fcbd0e063c809c3b6ac2e1b60bfad81
mdorelus-hub <mdorelus67@gmail.com>
2026-06-28 19:18:32 -0400

    Update analyze-food.js

--- api/analyze-food.js
+++ api/analyze-food.js
@@ -1,1 +1,10 @@
-
+export default function handler(req, res) {
+  return res.status(200).json({
+    success: true,
+    food: "backend working 🎉",
+    calories: 250,
+    protein: 10,
+    carbs: 30,
+    fat: 8
+  });
+}
